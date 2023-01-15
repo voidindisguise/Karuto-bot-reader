f85067485a5c1d1fa40f6ad8a10f20a20bd456d6
Shrikar <shrikar1601@gmail.com>
2023-01-15 10:53:19 +0530

    Multiple clients login

--- bot.py
+++ bot.py
@@ -1,34 +1,19 @@
-import discord, os, run_server, requests
+import requests
 
 from discord.ext import commands
 
-
-class disClient(commands.Bot):
+class DiscordClient(commands.Bot):
     def __init__(self):
         super().__init__(command_prefix=':')
         self.session = requests.Session()
-        self.token = os.getenv("TOKEN")
         self.remove_command('help')
-
+    
+    async def on_ready(self):
+        print("Hello")
+    
     async def on_message(self, message):
-        if message.author.id != 301856154332692480:
+        if message.author.id != self.user.id:
             await message.channel.send("Hello")
-            
-
-    def getHeaders(self):
-        headers = {
-            'Content-Type': 'application/json',
-            'User-Agent':
-            'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) discord/0.0.306 Chrome/78.0.3904.130 Electron/7.1.11 Safari/537.36',
-            'Authorization': self.token
-        }
-        return headers
-
-    def run(self):
-        super().run(self.token, bot=False)
 
 
-if __name__ == '__main__':
-    client = disClient()
-    run_server.keep_alive()
-    client.run()
+    
---
+++ main.py
@@ -0,0 +1,56 @@
+import asyncio
+import discord
+from collections import namedtuple
+from bot import DiscordClient
+
+async def ClientLogin(clients):
+    for _client in clients:
+        await _client.client.login(_client.token, bot=False)
+     
+async def WrappedConnect(_client):
+    try:
+        await _client.client.connect()
+    except Exception as e:
+        await _client.client.close()
+        print('We got an exception: ', e.__class__.__name__, e)
+        _client.event.set()
+        
+async def check_close(clients):
+    futures = [_client.event.wait() for _client in clients]
+    await asyncio.wait(futures)
+        
+
+if __name__ == '__main__':
+    clientsTuple = namedtuple('ClientsTuple', ['client', 'event', 'token'])
+
+    clients = [
+        clientsTuple(client=DiscordClient(), event=asyncio.Event(), token="MzAxODU2MTU0MzMyNjkyNDgw.G-QYIv.iNk3cieAhzoMpEcFl84f6cKSbU_ML9Rh5Qcey8"),
+        clientsTuple(client=DiscordClient(), event=asyncio.Event(), token="MzQ5NzQ4NDcxMjgyMDA4MDc0.GP2Fc4.2JL9ebzZHX0oCBp5cOlsEKNz_eMzOlLTsml33s"),
+        clientsTuple(client=DiscordClient(), event=asyncio.Event(), token="MzQ3NDEyNjc2OTgyNDcyNzA1.GeV7iP.ccmR40sn9Kq_L0r4gif-NRSG3C6t6rqxDzPq1g"),
+    ]
+
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(ClientLogin(clients))
+    
+    for client in clients:
+        loop.create_task(WrappedConnect(client))
+        
+    loop.run_until_complete(check_close(clients))
+    
+    loop.close()
+    
+    loop.run_forever()
+    
+    
+    
+    # tokens = [
+    #     "MzAxODU2MTU0MzMyNjkyNDgw.G-QYIv.iNk3cieAhzoMpEcFl84f6cKSbU_ML9Rh5Qcey8",
+    #     "MzQ5NzQ4NDcxMjgyMDA4MDc0.GP2Fc4.2JL9ebzZHX0oCBp5cOlsEKNz_eMzOlLTsml33s",
+    #     "MzQ3NDEyNjc2OTgyNDcyNzA1.GeV7iP.ccmR40sn9Kq_L0r4gif-NRSG3C6t6rqxDzPq1g",
+    #     "MzkyOTEyNjQ4NzE2NjgxMjE4.G23c7y.7Lavtb2_XeDiW6BaBAiCRcMNWSfpHxSrEcBuUc",
+    #     "MzE4NTAyNzA4ODk2MjAyNzUy.GFc7a5.QDATkX5OMYqQgS_k9oOW11vaIPqCaOxjbL9UHw",
+    #     "Mjk0Nzg2NTQ2MTgyMzg5NzYw.Gab33r.sLYu351EaLTrR1gklVmRG7zJsd_3n9KFw2OBG4",
+    #     "MzExNTgwMDY1ODExMDA1NDQw.Gnl7qG.jCbWWeGiJzq16yZqegWMDLTo32N4rL4FJ-nUZY",
+    #     "MzU1MzM0ODMxODIwODk4MzI1.GA4xNu.7Wlb450Fpht4TT3g-LWLTioOCEWV0-OWZdH63U",
+    #     "MzU1MjcwNDk2NTExODUyNTc2.GvJh_v.P9UxlbwAIu19g0HnyL10hG8AjE4J3aJKXS24MY"
+    # ]
